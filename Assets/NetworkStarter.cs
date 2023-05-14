c05975c72cf44a7856f7543d0a23581aaabcdeec
Viktor Kornilov <39262485+ViktorKornilov@users.noreply.github.com>
2023-05-14 12:35:39 +0300

    Connect UI

--- Assets/NetworkStarter.cs
+++ Assets/NetworkStarter.cs
@@ -1,5 +1,6 @@
 
 using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
 using UnityEngine;
 
 public class NetworkStarter : MonoBehaviour
@@ -9,8 +10,14 @@ public class NetworkStarter : MonoBehaviour
 
     private void Start()
     {
+        var transport = GetComponent<UnityTransport>();
+        transport.ConnectionData.Address = "localhost";
+        transport.ConnectionData.Port = 7777;
+        
+        
         var net = GetComponent<NetworkManager>();
         if (autoServer)net.StartHost();
         if (autoClient)net.StartClient();
+
     }
 }
